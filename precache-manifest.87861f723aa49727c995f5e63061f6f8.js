42374294b9b5808bb9e1255d5be7c2e80b1a694f
tanishqbiswas <tanishqbiswas@gmail.com>
2021-05-01 23:05:03 +0530

    Updates

---
+++ precache-manifest.87861f723aa49727c995f5e63061f6f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d9c543574413498c2ad2ac8ca26f95",
+    "url": "/recipeApp/index.html"
+  },
+  {
+    "revision": "9c121f1e272f7f21336f",
+    "url": "/recipeApp/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0ded3886a083d8ba0d83",
+    "url": "/recipeApp/static/css/main.de443d0b.chunk.css"
+  },
+  {
+    "revision": "9c121f1e272f7f21336f",
+    "url": "/recipeApp/static/js/2.a5864ee0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/recipeApp/static/js/2.a5864ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ded3886a083d8ba0d83",
+    "url": "/recipeApp/static/js/main.b084d849.chunk.js"
+  },
+  {
+    "revision": "2b7a515e344d60a70460",
+    "url": "/recipeApp/static/js/runtime-main.0a1dd118.js"
+  }
+]);
